f7361329bab39cc622f9e3805c2f743f5b5c57ab
Markleontyev <markleontyev123@gmail.com>
2025-07-05 20:53:22 +0400

    feat(Добавил докстринги к pipeline)

--- commands/pipeline.py
+++ commands/pipeline.py
@@ -1,10 +1,20 @@
-class CommandPipeline: 
-    def __init__(self): 
-        self.commands = [] 
+class CommandPipeline:
+    def __init__(self):
+        """
+        Инициализирует пустой конвейер команд.
+        """
+        self.commands = []
 
-    def add(self, command): 
+    def add(self, command):
+        """
+        Добавляет команду в конвейер.
+        """
         self.commands.append(command)
     
-    def run(self, data): 
-        for command in self.commands: data = command.execute(data) 
+    def run(self, data):
+        """
+        Последовательно выполняет все команды конвейера над данными.
+        """
+        for command in self.commands:
+            data = command.execute(data)
         return data
